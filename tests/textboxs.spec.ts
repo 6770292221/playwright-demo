8d38b38c2163c42752709095488ff26e2e80bc08
imaikungki <aphirak_2008@hotmail.com>
2025-06-28 23:11:52 +0700

    remove file

--- tests/textboxs.spec.ts
+++ tests/textboxs.spec.ts
@@ -1,37 +1,35 @@
-import { test, expect, Page } from "@playwright/test";
-
-const gotoInputPage = async (page: Page) => {
-  await page.goto("https://the-internet.herokuapp.com/inputs");
-};
-// ฟังก์ชันช่วยหา input
-const numberInput = (page) => page.locator("input[type='number']");
+import { test, expect } from "@playwright/test";
 
 // TC1: พิมพ์เลข 123 แล้วตรวจ
 test("Input accepts number 123", async ({ page }) => {
-  await gotoInputPage(page);
-  await numberInput(page).fill("123");
-  await expect(numberInput(page)).toHaveValue("123");
+  await page.goto("https://the-internet.herokuapp.com/inputs");
+  const input = page.locator("input[type='number']");
+  await input.fill("123");
+  await expect(input).toHaveValue("123");
 });
 
 // TC2: ลบค่า แล้วพิมพ์ -99
 test("Input accepts negative number -99", async ({ page }) => {
-  await gotoInputPage(page);
-  await numberInput(page).fill("-99");
-  await expect(numberInput(page)).toHaveValue("-99");
+  await page.goto("https://the-internet.herokuapp.com/inputs");
+  const input = page.locator("input[type='number']");
+  await input.fill("-99");
+  await expect(input).toHaveValue("-99");
 });
 
 // TC3: กด ArrowUp แล้วตรวจว่าเลขเพิ่ม
 test("ArrowUp increases the number", async ({ page }) => {
-  await gotoInputPage(page);
-  await numberInput(page).fill("10");
-  await numberInput(page).press("ArrowUp");
-  await expect(numberInput(page)).toHaveValue("11");
+  await page.goto("https://the-internet.herokuapp.com/inputs");
+  const input = page.locator("input[type='number']");
+  await input.fill("10");
+  await input.press("ArrowUp");
+  await expect(input).toHaveValue("11");
 });
 
 // TC4: กด ArrowDown แล้วตรวจว่าเลขลด
 test("ArrowDown decreases the number", async ({ page }) => {
-  await gotoInputPage(page);
-  await numberInput(page).fill("10");
-  await numberInput(page).press("ArrowDown");
-  await expect(numberInput(page)).toHaveValue("9");
+  await page.goto("https://the-internet.herokuapp.com/inputs");
+  const input = page.locator("input[type='number']");
+  await input.fill("10");
+  await input.press("ArrowDown");
+  await expect(input).toHaveValue("9");
 });
